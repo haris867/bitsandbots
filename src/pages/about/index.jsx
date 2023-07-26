de3967730f7900f722d2770474bb1c0c46e5c93f
Haris <haris.usman@hotmail.com>
2023-07-26 23:45:04 +0200

    Finished about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,18 +1,68 @@
+import { MainHeading } from "../../components/commonStyles/headings";
+import { Container, Col, Row } from "react-bootstrap";
 import {
-  MainHeading,
-  MainHeadingContainer,
-} from "../../components/commonStyles/headings";
-import { Container, Card, Col, Row } from "react-bootstrap";
+  AboutCard,
+  AboutCardImage,
+  AboutCardTitle,
+  AboutCardText,
+} from "./index.styles";
 
 export default function About() {
   return (
     <Container>
       <Col
-        md={11}
-        className="d-flex justify-content-between align-items-center mt-3 mb-0 mx-auto"
+        xs={12}
+        className="d-flex justify-content-between align-items-center my-3 mx-auto"
       >
         <MainHeading className="fs-2 fw-bold mb-0">About</MainHeading>
       </Col>
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} sm={10} md={8} lg={6} xl={5} className=" mb-4">
+          <AboutCard>
+            <div className="d-flex align-items-center">
+              <AboutCardTitle>About Us</AboutCardTitle>
+              <AboutCardImage src="/images/product-image.jpg" />
+            </div>
+            <AboutCardText>
+              Welcome to Bits & Bots, the vibrant universe for gamers where the
+              most fantastic, compelling, and high-quality video games are just
+              a click away. Founded in 2023, we are an online video game store
+              committed to fueling the passion of the gaming community with a
+              rich variety of games to choose from.
+            </AboutCardText>
+            <AboutCardText>
+              One key feature of Bits & Bots is our members-only access. We have
+              created an exclusive community of registered users, providing a
+              secure and dedicated platform for browsing and purchasing your
+              favorite games. Registration also allows us to tailor
+              recommendations based on your gaming preferences, bringing you one
+              step closer to your next gaming adventure.
+            </AboutCardText>
+          </AboutCard>
+        </Col>
+        <Col xs={12} sm={10} md={8} lg={6} xl={5} className=" mb-4">
+          <AboutCard>
+            <div className="d-flex align-items-center">
+              <AboutCardTitle>Our mission</AboutCardTitle>
+              <AboutCardImage src="/images/product-image2.jpg" />
+            </div>
+            <AboutCardText>
+              At Bits & Bots, our mission is simple. We aim to provide an
+              unrivaled experience to our members, offering an extensive catalog
+              of games that cater to every taste, genre, and platform. Whether
+              you're a dedicated fan of the classics or an enthusiast looking
+              for the next big thing in gaming, we have something for everyone.
+            </AboutCardText>
+            <AboutCardText>
+              Our team comprises passionate and seasoned gamers just like you.
+              This fuels our drive to maintain a user-centric approach in our
+              offerings, ensuring we meet your evolving gaming needs. We are
+              dedicated to creating a reliable, seamless, and exciting shopping
+              experience that goes beyond just a transaction.
+            </AboutCardText>
+          </AboutCard>
+        </Col>
+      </Row>
     </Container>
   );
 }
---
+++ src/pages/about/index.styles.js
@@ -0,0 +1,26 @@
+import { Card } from "react-bootstrap";
+import { styled } from "styled-components";
+
+export const AboutCard = styled(Card)`
+  background-color: var(--color-secondary);
+  border: none;
+  font-family: "Roboto Mono", sans-serif;
+`;
+
+export const AboutCardText = styled(Card.Text)`
+  font-size: calc(0.9rem + 0.3vw) !important;
+`;
+export const AboutCardTitle = styled(Card.Title)`
+  font-family: "Play", sans-serif;
+  color: var(--color-secondary);
+  position: absolute;
+  z-index: 1;
+  font-size: calc(1.7rem + 1.2vw) !important;
+  width: 100%;
+  text-align: center;
+`;
+export const AboutCardImage = styled(Card.Img)`
+  border-radius: 0 !important;
+  margin-bottom: 20px;
+  filter: brightness(0.4);
+`;
