7d8f7b12a93465f61ea733b7b08b0ab04ffb73fd
Haris <haris.usman@hotmail.com>
2023-08-03 05:52:47 +0200

    Added fetch url

--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -6,12 +6,16 @@ import { LoadingSpinner } from "../../components/commonStyles/loadingSpinner";
 export default function Game() {
   let { id } = useParams();
 
-  const singleGameUrl = `https://qg8g236v.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22Products%22+%26%26+_id+%3D%3D+%22${id}%22%5D+%7B%0A++%22name%22%3A+name%2C%0A++%22price%22%3A+price%2C%0A++%22rating%22%3A+rating%2C%0A++%22imageUrl%22%3A+image.asset-%3Eurl%2C%0A++%22description%22%3A+description%2C%0A++%22genres%22%3A+genres%0A%7D`;
+  const singleGameUrl = `https://qg8g236v.api.sanity.io/v2021-10-21/data/query/production?query=%7B%0A++%22product%22%3A+*%5B_type+%3D%3D+%22products%22+%26%26+_id+%3D%3D+%22${id}%22%5D+%7B%0A++++_id%2C%0A++++description%2C%0A++++%22imageUrl%22%3A+image.asset-%3Eurl%2C%0A++++name%2C%0A++++price%2C%0A++++rating%2C%0A++++%22genres%22%3A+genres%5B%5D-%3E%7B%0A++++++_id%2C%0A++++++genre%0A++++%7D%0A++%7D%0A%7D`;
 
   const { data, isFetchLoading, isFetchError } = useGetData(singleGameUrl);
 
-  var singleGame = data[0];
-  console.log(singleGame);
+  if (data.product) {
+    var singleGame = data.product[0];
+  } else {
+    singleGame = [];
+  }
+
   if (isFetchLoading) {
     return (
       <div className="mt-3 d-flex justify-content-center">
