043eb80511c17b63c4dcf7ae843dde3adaf7af18
Haris <haris.usman@hotmail.com>
2023-08-03 07:36:21 +0200

    Added Authorization functions

--- src/components/authModal/index.jsx
+++ src/components/authModal/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { Row, Col } from "react-bootstrap";
 import { styled } from "styled-components";
 import * as S from "./index.styles";
 import Tab from "react-bootstrap/Tab";
 import { PrimaryButton } from "../commonStyles/buttons";
+import { load, save } from "../../hooks/storage";
 
 const AuthFormContainer = styled(Col)`
   min-height: 250px;
@@ -18,28 +19,80 @@ const AuthFormContainer = styled(Col)`
 `;
 
 export default function AuthModal() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register: registerLogin,
+    handleSubmit: handleLoginSubmit,
+    reset: resetLogin,
+  } = useForm();
+  const {
+    register: registerRegister,
+    handleSubmit: handleRegisterSubmit,
+    reset: resetRegister,
+  } = useForm();
+
+  const [activeTab, setActiveTab] = useState("login");
+  const [loginFormMessage, setLoginFormMessage] = useState(false);
+  const [registerFormValidated, setRegisterFormValidated] = useState(false);
 
   function onLoginSubmit(data) {
-    console.log(data);
+    let users = load("users");
+    users = users ? JSON.parse(users) : [];
+
+    const user = users.find(
+      (user) =>
+        user.email.toLowerCase() === data.email.toLowerCase() &&
+        user.password === data.password
+    );
+
+    if (user) {
+      save("loggedInUser", JSON.stringify(user));
+      resetLogin();
+      setLoginFormMessage("Logging in...");
+      setTimeout(() => {
+        window.location.href = `/games`;
+      }, 1500);
+    } else {
+      setLoginFormMessage("Invalid email or password");
+    }
   }
   function onRegisterSubmit(data) {
-    console.log(data);
+    const newUser = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+
+    var users = load("users");
+    users = users ? JSON.parse(users) : [];
+
+    console.log(users);
+
+    users.push(newUser);
+
+    save("users", JSON.stringify(users));
+    resetRegister();
+    setTimeout(() => {
+      setActiveTab("login");
+    }, 2000);
+    setRegisterFormValidated(true);
   }
 
   return (
     <Row className="m-0">
       <AuthFormContainer xs={8} sm={6} md={4}>
         <S.AuthTabs
-          defaultActiveKey="login"
+          activeKey={activeTab}
+          onSelect={(t) => setActiveTab(t)}
           id="uncontrolled-tab-example"
           className="mb-2 auth-tabs"
         >
           <Tab eventKey="login" title="LOGIN">
             <form
               className="d-flex flex-column align-items-center"
-              onSubmit={handleSubmit(onLoginSubmit)}
+              onSubmit={handleLoginSubmit(onLoginSubmit)}
             >
+              <div>{loginFormMessage}</div>
+              {/* {loginFormValidated && <div>Logging in...</div>} */}
               <S.AuthFormLabel className="mb-1" htmlFor="email">
                 Email
               </S.AuthFormLabel>
@@ -47,7 +100,7 @@ export default function AuthModal() {
                 className="mb-2"
                 type="email"
                 placeholder="Your email address"
-                {...register("email")}
+                {...registerLogin("email")}
                 required={true}
                 // pattern="^[\w\-.]+@(stud\.)?noroff\.no$"
                 title="The email value must be a valid email"
@@ -59,8 +112,9 @@ export default function AuthModal() {
                 className="mb-2"
                 type="password"
                 placeholder="Your password"
-                {...register("password")}
+                {...registerLogin("password")}
                 required={true}
+                title="The password must be at least 8 characters"
                 minLength="8"
               />
               <PrimaryButton className="mt-2 my-4" type="submit">
@@ -71,8 +125,9 @@ export default function AuthModal() {
           <Tab eventKey="register" title="REGISTER">
             <form
               className="d-flex flex-column align-items-center"
-              onSubmit={handleSubmit(onRegisterSubmit)}
+              onSubmit={handleRegisterSubmit(onRegisterSubmit)}
             >
+              {registerFormValidated && <div>Registered successfully!</div>}
               <S.AuthFormLabel className="mb-1" htmlFor="name">
                 Full name
               </S.AuthFormLabel>
@@ -80,10 +135,10 @@ export default function AuthModal() {
                 className="mb-2"
                 type="text"
                 placeholder="Your full name"
-                {...register("name")}
+                {...registerRegister("name")}
                 required={true}
-                // pattern="^[\w\-.]+@(stud\.)?noroff\.no$"
-                title="The email value must be a valid email"
+                minLength={5}
+                title="The name must be at least 5 characters"
               />
               <S.AuthFormLabel className="mb-1" htmlFor="email">
                 Email
@@ -92,7 +147,7 @@ export default function AuthModal() {
                 className="mb-2"
                 type="email"
                 placeholder="Your email"
-                {...register("email")}
+                {...registerRegister("email")}
                 required={true}
                 // pattern="^[\w\-.]+@(stud\.)?noroff\.no$"
                 title="The email value must be a valid email"
@@ -104,8 +159,9 @@ export default function AuthModal() {
                 className="mb-2"
                 type="password"
                 placeholder="Your password"
-                {...register("password")}
+                {...registerRegister("password")}
                 required={true}
+                title="The password must be at least 8 characters"
                 minLength="8"
               />
               <PrimaryButton className="mt-2 my-4" type="submit">
